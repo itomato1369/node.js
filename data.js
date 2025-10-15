b550124b0999989f5bedb039618bebc0e26a167f
doowoo <itomato1369@gmail.com>
2025-10-15 15:40:18 +0900

    template literal

--- data.js
+++ data.js
@@ -10,7 +10,7 @@ function sum(a, b) {
   return a + b;
 }
 
-export { studentsAry, sum, PI };
+export { studentsAry, sum, getStudentInfo };
 // 여기에서 정의된 함수, 객체 및 기능들을 다른 JavaScript 에서 사용할 수 있도록
 // export 외부로 내보내겠다 계속 함수 정의하는 것 보다
 // 미리 만들어 놓고 export/import 하면 더 편하다
@@ -20,4 +20,3 @@ const PI = 3.14;
 function getStudentInfo() {
   return ["가나다라", "마바사", "아자차카", "파타하"];
 }
-getStudentInfo();
---
+++ literal.js
@@ -0,0 +1,43 @@
+// literal.js
+//
+import { getStudentInfo } from "./data.js";
+
+let myName = "기린";
+// console.log("Hello, " + myName);
+// console.log(`Bye,   ${myName}`);
+
+let n1 = 10;
+let n2 = 11;
+// console.log(`n1 + n2 = ${n1 + n2}`);
+
+// console.log(`${getStudentInfo().forEach((person) => console.log(person))}`);
+// console.log(
+//   `${getStudentInfo()
+//     .map((person) => "친구이름=>" + person + "\n")
+//     .join(" ")}`
+//   // return이 생략됨
+// );
+
+// 펼침연산자(spread operatior)
+let friends = ["애플", "삼성", "노키아"];
+// console.log(...friends);
+
+let newAry = [friends, getStudentInfo()];
+// console.log(newAry);
+
+// Object Destructuring
+// 객체를 분해 펼친다
+const person = {
+  firstName: "구글",
+  lastName: "마이크로",
+  age: 20,
+};
+
+// let firstName = person.firstName;
+let { firstName: fn, lastName: ln, age } = person;
+console.log(fn, ln, age);
+
+// Array Destructuring
+// "가나다라", "마바사", "아자차카", "파타하"
+let [ary1, ary2, ...ary3] = getStudentInfo();
+console.log(ary1, ary2, ary3);
